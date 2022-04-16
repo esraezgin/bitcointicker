0a5ea9163a346c4a31d82318ffb3ae041492173a
esra <esraezgin1234@gmail.com>
2022-04-16 17:23:02 +0300

    Coins list ui and detail ui added

--- app/src/main/java/com/android/app/loodos/bitcointicker/feature/coinlist/viewmodel/CoinsListViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.android.app.loodos.bitcointicker.feature.coinlist.viewmodel
-
-class CoinsListViewModel {
-}
---
+++ app/src/main/java/com/android/app/loodos/bitcointicker/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.android.app.loodos.bitcointicker.utils
+
+object Constants {
+}
